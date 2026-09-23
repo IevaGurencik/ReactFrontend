1732c2098b2676db704f3ef3afc84bc84947a008
ZanorNeon <raytherayoor@gmail.com>
2026-09-23 19:20:19 +0300

    Fix Elastic IP

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const isLocal = window.location.hostname === "localhost" || window.location.hostname === "16.16.227.125";
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "16.192.224.87";
 export const API_BASE = isLocal ? "http://localhost:8080" : `http://${window.location.hostname}:8080`;
 
 const api = axios.create({
